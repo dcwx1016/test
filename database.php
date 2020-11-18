e4950449a310cd3f502ef8bae9e24aed2688c435
Yang Zou <dcwx1016@gmail.com>
2020-11-18 16:44:06 +0000

    connect to db

--- database.php
+++ database.php
@@ -1,11 +1,22 @@
 <?php
-$connection = mysqli_connect('localhost','yangzou','123','auction_13');
+// $connection = mysqli_connect('localhost','yangzou','123','auction_13');
 
-function load_query($query){
-    global $connection;
-    $result = mysqli_query($connection, $query);
-    mysqli_close($connection);
-    return $result;
-}
+// function load_query($query){
+//     global $connection;
+//     $result = mysqli_query($connection, $query);
+//     mysqli_close($connection);
+//     return $result;
+// }
+
+$serverName = "auction13.database.windows.net"; // update me
+$connectionOptions = array(
+    "Database" => "auction13", // update me
+    "Uid" => "group13", // update me
+    "PWD" => "102465536Zz" // update me
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+$tsql= "SELECT * FROM Customers";
+$getResults= sqlsrv_query($conn, $tsql);
 
 
