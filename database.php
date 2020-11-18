21a74be36a44e07dbdfac45a3e2e40d9bda97a0e
Yang Zou <dcwx1016@gmail.com>
2020-11-18 16:47:57 +0000

    connect to db

--- database.php
+++ database.php
@@ -19,4 +19,12 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
 $tsql= "SELECT * FROM Customers";
 $getResults= sqlsrv_query($conn, $tsql);
 
+echo ("Reading data from table" . PHP_EOL);
+//if ($getResults == FALSE)
+//    echo (sqlsrv_errors());
+//while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+//    echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+//}
+//sqlsrv_free_stmt($getResults);
+
 
